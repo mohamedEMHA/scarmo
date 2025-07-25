78cdeef3660e82dfdfec46c9f38e1c1571ed6fd0
Mohamed Abdelkarim <83008296+mohamedEMHA@users.noreply.github.com>
2025-07-25 05:56:11 -0400

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -349,7 +349,7 @@ const Navigation = ({ currentSection }: NavigationProps) => {
             )}
             
             {/* Printful Cart Icon */}
-            <div className={isScrolled ? 'text-foreground' : 'text-black'}>
+            <div className={isScrolled ? 'text-foreground' : 'text-white'}>
               <PrintfulCartIcon />
             </div>
 
